490d599059b765165eb48b229a983eeb46436dc3
truongptit99 <xuantruong120499@gmail.com>
2023-11-23 22:10:36 +0700

    fake api + flexible components with slots and flags

--- js/components/AssignmentList.js
+++ js/components/AssignmentList.js
@@ -1,21 +1,49 @@
-import Assignment from "./Assignment.js"
+import Assignment from "./Assignment.js";
+import AssignmentTags from "./AssignmentTags.js";
 
 export default {
     components: {
-        Assignment
+        Assignment, AssignmentTags
     },
     template: 
     /* html */
     `
-        <section v-show="assignments.length">
-            <h2 class="font-bold mb-2">{{ title }}</h2>
-            <ul class="border border-gray-600 divide-y divide-gray-600">
-                <assignment :assignment="assignment" v-for="assignment in assignments" :key="assignment.id"></assignment>
+        <section v-show="show && assignments.length" class="w-60">
+            <div class="flex justify-between">
+                <h2 class="font-bold mb-2">
+                    {{ title }}
+                    <span>({{ filteredAssignments.length }})</span>
+                </h2>
+                <button v-show="canToggle" @click="$emit('toggle')">&times;</button>
+            </div>
+            <assignment-tags
+                :initial-tags="assignments.map(a => a.tag)"
+                v-model:currentTag="currentTag"
+            />
+            <ul class="border border-gray-600 divide-y divide-gray-600 mt-6">
+                <assignment :assignment="assignment" v-for="assignment in filteredAssignments" :key="assignment.id"></assignment>
             </ul>
+
+            <slot></slot>
         </section>
     `,
     props: {
         assignments: Array,
-        title: String
+        title: String,
+        canToggle: { type: Boolean, default: false }
+    },
+    data() {
+        return {
+            currentTag: 'all',
+            show: true
+        }
+    },
+    computed: {
+        filteredAssignments() {
+            if (this.currentTag === 'all') {
+                return this.assignments;
+            }
+            return this.assignments.filter(a => a.tag === this.currentTag);
+        }
     }
 }
--- js/components/Assignments.js
+++ js/components/Assignments.js
@@ -9,10 +9,18 @@ export default {
     template:
     /* html */
     `
-        <section class="space-y-6">
-            <assignment-list :assignments="filters.inProgress" title="In progress"></assignment-list>
-            <assignment-list :assignments="filters.completed" title="Completed"></assignment-list>
-            <assignment-create :assignments="assignments" @add="add"></assignment-create>            
+        <section class="flex gap-8">
+            <assignment-list :assignments="filters.inProgress" title="In progress">
+                <assignment-create :assignments="assignments" @add="addNew"></assignment-create>
+            </assignment-list>
+            <div v-show="showCompleted">
+                <assignment-list
+                    :assignments="filters.completed"
+                    title="Completed"
+                    can-toggle
+                    @toggle="showCompleted = !showCompleted"
+                ></assignment-list>            
+            </div>
         </section>
     `,
     data() {
@@ -21,10 +29,11 @@ export default {
             buttonClasses: 'text-green',
             active: false,
             assignments: [
-                { name: 'Finish project', complete: false, id: 1 },
-                { name: 'Read Chapter 4', complete: false, id: 2 },
-                { name: 'Turn in Homework', complete: false, id: 3 },
+                // { name: 'Finish project', complete: false, id: 1, tag: 'math' },
+                // { name: 'Read Chapter 4', complete: false, id: 2, tag: 'science' },
+                // { name: 'Turn in Homework', complete: false, id: 3, tag: 'math' },
             ],
+            showCompleted: true
         }
     },
     computed: {
@@ -41,12 +50,20 @@ export default {
             }
         }
     },
+    created() {
+        fetch('http://localhost:3001/assignments')
+            .then(response => response.json())
+            .then(assignments => {
+                this.assignments = assignments;
+            })
+    },
     methods: {
-        add(name) {
+        addNew(name) {
             this.assignments.push({
                 name: name,
                 completed: false,
-                id: this.assignments.length + 1
+                id: this.assignments.length + 1,
+                tag: 'new'
             });
         }
     }
